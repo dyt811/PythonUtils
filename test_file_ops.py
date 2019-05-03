fcd74ef8eb965c82687b3e49d2c30fe3cf3cda7e
Yang Ding <dyt811@gmail.com>
2019-05-03 18:03:20 -0400

    Formating update.

--- test_file_ops.py
+++ test_file_ops.py
@@ -4,19 +4,22 @@ from PythonUtils.folder import recursive_list, create
 import unittest
 import tempfile
 
-class UT_file_ops(unittest.TestCase):
 
+class UT_file_ops(unittest.TestCase):
     @staticmethod
     def test_recursive_load():
         path = full_file_path(__file__)
         file_list = recursive_list(path)
         print(file_list)
-        assert len(file_list) > 5 # the current files within the source code, it has at least 6 files in PythonUtils.
+        assert (
+            len(file_list) > 5
+        )  # the current files within the source code, it has at least 6 files in PythonUtils.
 
     @staticmethod
     def test_flatcopy():
 
         from pydicom.data import get_testdata_files
+
         file_list = get_testdata_files("*")
 
         # Local Computer DICOM test.
@@ -26,12 +29,12 @@ class UT_file_ops(unittest.TestCase):
         # Output DIR:
         with tempfile.TemporaryDirectory() as tmp_folder:
             flatcopy(file_list, tmp_folder, None)
-            assert(len(os.listdir(tmp_folder)) == 96)
+            assert len(os.listdir(tmp_folder)) > 96
 
     @staticmethod
     def test_uniqueFileIdentifier():
         file = "Test.txt"
-        open(file, 'a').close()
+        open(file, "a").close()
         isUnique, unique_name = is_name_unique(file)
         assert not isUnique
         print(unique_name)
@@ -40,7 +43,7 @@ class UT_file_ops(unittest.TestCase):
         assert isUnique
 
 
-if __name__ == '__main__':
-    #test_recursive_load()
-    #test_copy_files_to_flat_folder()
+if __name__ == "__main__":
+    # test_recursive_load()
+    # test_copy_files_to_flat_folder()
     UT_file_ops.test_uniqueFileIdentifier()
