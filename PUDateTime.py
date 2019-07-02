51e7467e84ddf0b429126ef90c0193d38f18908c
Yang Ding <dyt811@gmail.com>
2019-07-02 14:14:30 -0400

    Add delay execution function

--- PUDateTime.py
+++ PUDateTime.py
@@ -1,5 +1,38 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, time as timeobject
 
+
+def sleep_until(destination_time: timeobject):
+    """
+    Sleep until particular time of the date.
+    :param destination_time:
+    :return:
+    """
+    sleep_duration = calculate_countdown(datetime.now(), destination_time).total_seconds()
+    import time
+    time.sleep(sleep_duration)
+
+def calculate_countdown(source_datetime:datetime, destination_time:timeobject) -> timedelta:
+    """
+    Compute the time delta between the current time and to the same destination/hour/time of today.
+    Used primarily in scheduled function to compute time delta until next run.
+    :param source_datetime:
+    :param destination_time:
+    :return:
+    """
+    # Get the date from the source datetime
+    destination_date: datetime.date = source_datetime.date()
+
+    # Compute the destination date time by cominging the destination DATE (e.g. today) with the desired destination time.
+    destination_datetime: datetime = datetime.combine(destination_date, destination_time)
+
+    # Add one day if the current destination date time is in the past: used to prevent NEGATIVE countdowns.
+    if destination_datetime < source_datetime:
+        destination_datetime = datetime.combine(destination_date+timedelta(days=1), destination_time)
+
+    # Compute the delta.
+    remaining_time: timedelta = destination_datetime - source_datetime
+
+    return remaining_time
 
 def datetime_increment(
     datetime_input: datetime, number_of_unit_hours_increment: int, unit_hours: int = 1
@@ -66,4 +99,6 @@ def iso2tstr(input_datetime: datetime) -> str:
 
 
 if __name__ == "__main__":
-    print(datetime_increment(datetime.now(), 5))
+    from datetime import time as timeobject
+    sleep_duration = calculate_countdown(datetime.now(), timeobject(hour=19)).total_seconds()
+    print(f"{sleep_duration} seconds until waking up.")
