657f0f87cee652de8ab5370f17c0c95d6073b1d4
Yang Ding <dyt811@gmail.com>
2019-09-17 19:02:49 -0400

    Black format update
    BLACK ensures we are compliant with best Python formats.

--- rle.py
+++ rle.py
@@ -4,13 +4,16 @@ from collections import namedtuple
 # Run Length Decoding.
 # This is a specialized module to make it less PITA.
 
-class RLE: # Rune Length Encoding
-    def __init__(self,
-                 order:List[int],
-                 length:List[int],
-                 x_max=1600,
-                 y_max=256,
-                 y_encoded_first=True):
+
+class RLE:  # Rune Length Encoding
+    def __init__(
+        self,
+        order: List[int],
+        length: List[int],
+        x_max=1600,
+        y_max=256,
+        y_encoded_first=True,
+    ):
         """
         Establish the assumption before encoding and decoding.
         :param order: the Nth pixel count.
@@ -19,7 +22,9 @@ class RLE: # Rune Length Encoding
         :param y_max: max y values (number of rows)
         :param y_encoded_first:
         """
-        self.y_encoded_first = y_encoded_first # assuming Y direction is incremented first before incrementing X in terms of pixel indexing.
+        self.y_encoded_first = (
+            y_encoded_first
+        )  # assuming Y direction is incremented first before incrementing X in terms of pixel indexing.
         self.y_max = y_max
         self.x_max = x_max
         self.list_order = order
@@ -41,7 +46,7 @@ class RLE: # Rune Length Encoding
         for index, length in enumerate(self.list_length):
 
             # Run-Length i.e. Order + Length pair return a list of pixels
-            list_xy = self.decode_OrderLength_to_listxy(self.list_order[index], length )
+            list_xy = self.decode_OrderLength_to_listxy(self.list_order[index], length)
 
             # Append to the total pixels.
             total_xy = total_xy + list_xy
@@ -50,20 +55,23 @@ class RLE: # Rune Length Encoding
 
     def decode_OrderLength_to_listxy(self, order: int, length: int) -> List[tuple]:
         list_xy = []
-        for index in range(order,order+length):
+        for index in range(order, order + length):
             list_xy.append(self.decode_order_to_xy(index))
         return list_xy
 
-    def decode_order_to_xy(self, pixel_order:int) -> tuple:
+    def decode_order_to_xy(self, pixel_order: int) -> tuple:
         """
         Take an order integer in a 1600 x 255 images, convert it to xy coordinate in the form of a Pixel object
         :param pixel_order: integer order of the pixel
         :return: Pixel object
         """
-        x = (pixel_order - 1) // self.y_max + 1 # had to offset pixel order because orders are 1 based.
-        y = (pixel_order - 1) % self.y_max + 1 #
+        x = (
+            pixel_order - 1
+        ) // self.y_max + 1  # had to offset pixel order because orders are 1 based.
+        y = (pixel_order - 1) % self.y_max + 1  #
         return (x, y)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     alpha = RLE([255, 1231], [5, 12])
     print(alpha.decode())
